79b389caba12954eddb4ff83191db1795b190966
Rachelle <rachellecha@users.noreply.github.com>
2020-07-25 16:10:21 -0400

    almost done with the basis of the file

--- spotify.py
+++ spotify.py
@@ -23,10 +23,40 @@ except:
 spotifyObject = spotipy.Spotify(auth=token)
 
 user = spotifyObject.current_user()
-dispayName = user["display_name"]
+displayName = user["display_name"]
 
+while True:
 
+    print()
+    print("Welcome to Chromesthesia " + displayName)
+    print()
+    print("0 - Search for Song")
+    print("1 - exit")
+    print()
+    choice = input("Your choice ")
 
+#Search for song
+    if choice == "0":
+        print()
+        searchQuery = input("What's the song: ")
+        print()
+
+        song = spotifyObject.search(searchQuery, 1, 0, "track")
+
+        artist = song["tracks"]["items"][0]["artists"][0]["name"]
+
+        searchResults = spotifyObject.search(artist, 1, 0, "artist")
+
+        genre = searchResults["artists"]["items"][0]
+
+        print(genre["genres"])
+
+        #print(json.dumps(searchResults, sort_keys = True, indent=4))
+        
+
+#end program
+    if choice == "1":
+        break
 
 # print(json.dumps(VARIABLE, sort_keys = True, indent=4))
 
