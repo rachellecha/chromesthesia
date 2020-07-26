acd03bcea2067b6f7bb643ee15d120d559661fc2
Rachelle <rachellecha@users.noreply.github.com>
2020-07-25 22:13:56 -0400

    just updating whatever I have

--- spotify.py
+++ spotify.py
@@ -5,10 +5,9 @@ import webbrowser
 import json
 import spotipy.util as util
 from json.decoder import JSONDecodeError
-#from __builtin__ import any as b_any
 
 # get username from terminal
-username = sys.argv[1]
+username = "12171678313"
 
 #12171678313
 
@@ -23,62 +22,48 @@ except:
 #create spotify object
 spotifyObject = spotipy.Spotify(auth=token)
 
-user = spotifyObject.current_user()
-displayName = user["display_name"]
-
-while True:
-
-    print()
-    print("Welcome to Chromesthesia " + displayName)
-    print()
-    print("0 - Search for Song")
-    print("1 - exit")
-    print()
-    choice = input("Your choice ")
+#user = spotifyObject.current_user()
+#displayName = user["display_name"]
 
 #Search for song
-    if choice == "0":
-        print()
-        searchQuery = input("What's the song: ")
-        print()
+def colorPicker(input):
 
-        song = spotifyObject.search(searchQuery, 1, 0, "track")
+    song = spotifyObject.search(input, 1, 0, "track")
 
-        artist = song["tracks"]["items"][0]["artists"][0]["name"]
+    artist = song["tracks"]["items"][0]["artists"][0]["name"]
 
-        searchResults = spotifyObject.search(artist, 1, 0, "artist")
+    searchResults = spotifyObject.search(artist, 1, 0, "artist")
 
-        genre = searchResults["artists"]["items"][0]["genres"]
+    genre = searchResults["artists"]["items"][0]["genres"]
 
         #print(type(genre))
 
-        print(genre)
+        #print(genre)
 
         #print(json.dumps(searchResults, sort_keys = True, indent=4))
+
+    if any("indie" in genre for genre in genre):
+        print("yellow")
+
+    if "pop" in genre:
+        print("pink")
         
-        if any("indie" in genre for genre in genre):
-            print("yellow")
+    if "folk" in genre:
+        print("yellow")
 
-        if "pop" in genre:
-            print("pink")
+    if "rap" in genre:
+        print("black")
+
+    if "alternative r&b" in genre:
+        print("pink")
         
-        if "folk" in genre:
-            print("yellow")
+    if "pop punk" in genre:
+        print("blue")
 
-        if "rap" in genre:
-            print("black")
+input = input("Song Name? ")
 
-        if "alternative r&b" in genre:
-            print("pink")
-        
-        if "pop punk" in genre:
-            print("blue")
-
-
-
-#end program
-    if choice == "1":
-        break
+colorPicker(input)
 
 # print(json.dumps(VARIABLE, sort_keys = True, indent=4))
+
 
--- website.py
+++ website.py
@@ -1,4 +1,4 @@
-from flask import Flask, redirect, url_for, render_template
+from flask import Flask, redirect, url_for, render_template, request, flash
 
 app = Flask(__name__)
 
