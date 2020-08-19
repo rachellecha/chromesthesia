2053a85965546a4b0474ad550f6f430e47f5bf5d
Rachelle <rachellecha@users.noreply.github.com>
2020-08-19 12:36:50 -0400

    added google sheets integreation which is conncted to my google forms

--- spotify.py
+++ spotify.py
@@ -5,6 +5,9 @@ import webbrowser
 import json
 import spotipy.util as util
 from json.decoder import JSONDecodeError
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
+
 
 # get username from terminal
 username = "12171678313"
@@ -25,44 +28,61 @@ spotifyObject = spotipy.Spotify(auth=token)
 #user = spotifyObject.current_user()
 #displayName = user["display_name"]
 
+#connect google sheets
+
+scope = ["https://spreadsheets.google.com/feeds", "https://www.googleapis.com/auth/drive"]
+
+c = ServiceAccountCredentials.from_json_keyfile_name("creds.json", scope)
+
+gc = gspread.authorize(c)
+
+sh = gc.open("Chromesthesia").sheet1
+
+songsCol = sh.col_values(2)
+colorsCol = sh.col_values(15)
+
+print(colorsCol)
+
+#print(sh.get_all_records())
+
 #Search for song
-def colorPicker(input):
-
-    song = spotifyObject.search(input, 1, 0, "track")
-
-    artist = song["tracks"]["items"][0]["artists"][0]["name"]
-
-    searchResults = spotifyObject.search(artist, 1, 0, "artist")
-
-    genre = searchResults["artists"]["items"][0]["genres"]
-
-        #print(type(genre))
-
-    #print(genre)
-
-    print(json.dumps(song, sort_keys = True, indent=4))
-
-    if any("indie" in genre for genre in genre):
-        print("yellow")
-
-    if "pop" in genre:
-        print("pink")
-        
-    if "folk" in genre:
-        print("yellow")
-
-    if "rap" in genre:
-        print("black")
-
-    if "alternative r&b" in genre:
-        print("pink")
-        
-    if "pop punk" in genre:
-        print("blue")
-
 input = input("Song Name? ")
 
-colorPicker(input)
+song = spotifyObject.search(input, 1, 0, "track")
+
+#print(json.dumps(song, sort_keys = True, indent=4))
+
+trackURI = song["tracks"]["items"][0]["uri"]
+
+searchResults = spotifyObject.audio_features(trackURI)
+
+#genre = searchResults["artists"]["items"][0]["genres"]
+
+#print(type(genre))
+
+#print(genre)
+
+print(json.dumps(searchResults, sort_keys = True, indent=4))
+
+#if any("indie" in genre for genre in genre):
+    #print("yellow")
+
+#if "pop" in genre:
+    #print("pink")
+        
+#if "folk" in genre:
+    #print("yellow")
+
+#if "rap" in genre:
+    #print("black")
+
+#if "alternative r&b" in genre:
+    #print("pink")
+        
+#if "pop punk" in genre:
+    #print("blue")
+
+
 
 # print(json.dumps(VARIABLE, sort_keys = True, indent=4))
 
